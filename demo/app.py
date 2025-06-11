9199ededaa4072aab687f3eefb52df822c33203f
Vento Li <ventoli@Ventos-MacBook-Air.local>
2025-06-10 18:36:26 -0700

    template html

--- demo/app.py
+++ demo/app.py
@@ -1,24 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-# Dynamic user route
-@app.route('/user/<username>')
-def user_profile(username):
-    return render_template('user.html', username=username)
-
-# Form input handling
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        return render_template('greet.html', name=name)
-    return render_template('greet.html', name=None)
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
