7c88f8af1fcc0c6706e4849b43be5ab034903a1b
Vento Li <ventodx5@gmail.com>
2025-06-17 12:22:45 -0700

    sql example

--- demo/app.py
+++ demo/app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, jsonify
+from config import DATABASE_URI
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+db.init_app(app)
 
-@app.route('/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
+# Create tables at startup
+with app.app_context():
+    db.create_all()
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.json.get('username')
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    user = User(username=username)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": f"User {username} added."})
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{"id": u.id, "username": u.username} for u in users])
 
 if __name__ == '__main__':
     app.run(debug=True)
